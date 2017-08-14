fb735274b2c4b5040b08b165cffbe01efa9c4ee4
arsenico95 <arsenico_z33@yahoo.com>
2017-08-14 10:02:58 -0500

    Initial Commit

--- Source/Inixe.CoinManagement.Common/IMarket.cs
+++ Source/Inixe.CoinManagement.Common/IMarket.cs
@@ -1,13 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="IMarket.cs" company="Inixe">
+// Copyright (c) Inixe 2017. All rights reserved.
+// </copyright>
 
 namespace Inixe.CoinManagement.Common
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// IMaket Interface
+    /// </summary>
     public interface IMarket
     {
-
     }
 }
